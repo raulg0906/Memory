b01cd680224300bed505e8095de11725f09dd2bb
Raul Gonzalez <raulgonzalez1770@gmail.com>
2019-04-07 11:59:17 -0700

    updated memory game

--- game.js
+++ game.js
@@ -1,32 +1,63 @@
 class MemoryGame {
     constructor(){
         this._lives = 10;
+        this._lifeBarArr = [
+            `<i class="fas fa-heart"></i>`,
+            `<i class="fas fa-heart"></i>`,
+            `<i class="fas fa-heart"></i>`,
+            `<i class="fas fa-heart"></i>`,
+            `<i class="fas fa-heart"></i>`,
+            `<i class="fas fa-heart"></i>`,
+            `<i class="fas fa-heart"></i>`,
+            `<i class="fas fa-heart"></i>`,
+            `<i class="fas fa-heart"></i>`,
+            `<i class="fas fa-heart"></i>`
+        ];
     }
 
     get lives() {
         return this._lives;
     }
 
-    shuffle(){
+    loseLife() {
+        this._lives--;
+        if (this._lives > 0) {
+            this.displayLife(-1);
+        }
+        else {
+            gameOver();
+        }
+    }
+
+    displayLife(lostLife) {
+        if (lostLife) {
+            this._lifeBarArr.pop();
+        }
+        var lifeBarContainer = document.querySelector("#hearts");
+        var lifeBarStr = this._lifeBarArr.join("");
+        lifeBarContainer.innerHTML = lifeBarStr;
+    }
+
+    shuffle() {
         var iconArr = [
-            `<i class="hidden far fa-angry"></i>`,
-            `<i class="hidden far fa-angry"></i>`,
-            `<i class="hidden fas fa-battery-empty"></i>`,
-            `<i class="hidden fas fa-battery-empty"></i>`,
-            `<i class="hidden fas fa-atom"></i>`,
-            `<i class="hidden fas fa-atom"></i>`,
-            `<i class="hidden far fa-bell"></i>`,
-            `<i class="hidden far fa-bell"></i>`,
-            `<i class="hidden fas fa-bacon"></i>`,
-            `<i class="hidden fas fa-bacon"></i>`,
-            `<i class="hidden fab fa-battle-net"></i>`,
-            `<i class="hidden fab fa-battle-net"></i>`,
-            `<i class="hidden fas fa-car"></i>`,
-            `<i class="hidden fas fa-car"></i>`,
-            `<i class="hidden fas fa-cannabis"></i>`,
-            `<i class="hidden fas fa-cannabis"></i>`,
-            `<i class="hidden fas fa-child"></i>`,
-            `<i class="hidden fas fa-child"></i>`
+            `<i class="far fa-angry hidden"><span>a</span></i>`,
+            `<i class="far fa-angry hidden"><span>a</span></i>`,
+            `<i class="fas fa-battery-empty hidden"><span>b</span></i>`,
+            `<i class="fas fa-battery-empty hidden"><span>b</span></i>`,
+            `<i class="fas fa-atom hidden"><span>c</span></i>`,
+            `<i class="fas fa-atom hidden"><span>c</span></i>`,
+            `<i class="far fa-bell hidden"><span>d</span></i>`,
+            `<i class="far fa-bell hidden"><span>d</span></i>`,
+            `<i class="fas fa-bacon hidden"><span>e</span></i>`,
+            `<i class="fas fa-bacon hidden"><span>e</span></i>`,
+            `<i class="fab fa-battle-net hidden"><span>f</span></i>`,
+            `<i class="fab fa-battle-net hidden"><span>f</span></i>`,
+            `<i class="fas fa-car hidden"><span>g</span></i>`,
+            `<i class="fas fa-car hidden"><span>g</span></i>`,
+            `<i class="fas fa-cannabis hidden"><span>h</span></i>`,
+            `<i class="fas fa-cannabis hidden"><span>h</span></i>`,
+            `<i class="fas fa-child hidden"><span>i</span></i>`,
+            `<i class="fas fa-child hidden"><span>i</span></i>`
         ];
         var iconArrLength = iconArr.length;
         var gameTilesString = '';
@@ -42,4 +73,27 @@ class MemoryGame {
         var gameBoardContainer = document.querySelector("#gameboard");
         gameBoardContainer.innerHTML = gameTilesString;
     }
+
+    checkMatch(selection1, selection2) {
+
+        if (selection1[0].innerHTML === selection2[0].innerHTML) {
+            selection1.removeClass("selected");
+            selection1.addClass("matched");
+            selection2.removeClass("selected");
+            selection2.addClass("matched");
+        }
+        else {
+            this.loseLife();
+            selection1.removeClass("selected");
+            selection2.removeClass("selected");
+            setTimeout(() => {   
+                selection1.addClass("hidden");
+                selection2.addClass("hidden");
+            }, 500);
+        }
+
+        
+
+    }
+
 }
--- main.js
+++ main.js
@@ -1,22 +1,52 @@
 $(document).ready(function(){
 
     var game = new MemoryGame;
+    game.displayLife();
     game.shuffle();
 
+    var selection1;
+    var selection2;
 
     $("#gameboard").on('click', '.gametile', function(){
 
         var content =$(this).children('i')
-        var tileClassStr = content.attr('class')
-        console.log(tileClassStr)
-        if(content.hasClass("hidden")){
-            content.removeClass("hidden")
+
+        if (!(selection1)) {
+
+            selection1 = content;
+            if (selection1.hasClass("selected") || selection1.hasClass("matched")) {
+                selection1 = false;
+            }
+            else {
+                cardCheck(selection1);
+            }
+
+        } else {
+
+            selection2 = content;
+            if (selection2.hasClass("selected") || selection2.hasClass("matched")) {
+                selection2 = false;
+            }
+            else {
+                cardCheck(selection2);
+            }
 
         }
-        // else if(){
-        //     content.addClass("matched")
-        // }
+
+        if (selection1 && selection2) {
+            game.checkMatch(selection1, selection2);
+            selection1 = false;
+            selection2 = false;
+        }
         
     })
+
+    function cardCheck(card) {
+
+        if (card.hasClass("hidden")) {
+            card.removeClass("hidden");
+            card.addClass("selected");
+        }
+    }
     
 })
